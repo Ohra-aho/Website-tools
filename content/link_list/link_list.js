ad3663d0b5446be0bab6fd0d8886363358710686
Panu Ohra-aho <panu.ohraaho@gmail.com>
2025-12-17 15:38:34 +0200

    link list compleated and adjusted

--- content/link_list/link_list.js
+++ content/link_list/link_list.js
@@ -13,13 +13,21 @@ class LinkList extends HTMLElement {
 		const template = document.getElementById("link-list-template");
 		for(let i = 0; i < this.links.length; i++) {
 			template.content.appendChild(this.links[i]);
+			template.content.children[0].appendChild(this.links[i].cloneNode(true));
 		}
 		this.appendChild(template.content.cloneNode(true));
 		this.querySelector("button").onclick = this.OpenList;
+		this.querySelector("ham-list").style.top = `-${this.querySelector("ham-list").offsetHeight}px`;
 	}
 
 	OpenList() {
-		console.log("toimii");
+		this.parentElement.classList.toggle("open")
+		if(this.parentElement.classList.contains("open")) {
+			this.parentElement.querySelector("ham-list").style.top = `100%`;
+		} else {
+			this.parentElement.querySelector("ham-list").style.top = `-${this.parentElement.querySelector("ham-list").offsetHeight}px`;
+		}
+		
 	}
 }
 
