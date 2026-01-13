4e7109b3beb40602614e8af9a11703b6bdf185c6
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-01-13 16:55:20 +0200

    sticky header redundancy removed

--- main/header/header.js
+++ main/header/header.js
@@ -1,9 +1,9 @@
 const sticky_header_template = document.createElement("template");
 sticky_header_template.innerHTML = 
 `
-<header-sticky>
-	<button id="header-open">&#8595;</button>
-</header-sticky>
+
+<button id="header-open">&#8595;</button>
+
 `
 
 
@@ -16,20 +16,22 @@ class StickyHeader extends HTMLElement {
 	connectedCallback() { 
 		const height = this.offsetHeight;
 		this.appendChild(sticky_header_template.content.cloneNode(true));
+
 		let button = this.querySelector("button");
 		button.onclick = this.Open;
+
 		this.style.top = `-${height}px`;
-		window.addEventListener("resize", (event) => { 
+
+		window.addEventListener("resize", () => { 
 			if(!Array.from(this.classList).includes("open")) {
 				const height = this.offsetHeight; 
 				this.style.top = `-${height}px`;
 			}
-			
 		})
 	}
 
 	Open() {
-		const parent = this.parentElement.parentElement;
+		const parent = this.parentElement;
 		if(parent.classList.toggle("open")) {
 			parent.style.top = `0`;
 			this.innerHTML = "&#8593;";
