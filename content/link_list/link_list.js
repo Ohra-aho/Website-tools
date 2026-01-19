20ecb85a7b5d37426d643e42f4f335b1cfe85804
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-01-19 16:29:29 +0200

    Link list issue fixed

--- content/link_list/link_list.js
+++ content/link_list/link_list.js
@@ -1,12 +1,19 @@
 const link_list_template = document.createElement("template");
 link_list_template.innerHTML = 
 `
-<ham-list>
-		
-</ham-list>
 <button>
 	<ham-icon></ham-icon>
 </button>
+`
+
+const ham_list_temp = document.createElement("template");
+ham_list_temp.innerHTML = 
+`
+<ham-list>
+	<div>
+		<button>&#x2715;</button>
+	</div>
+</ham-list>
 `
 
 class LinkList extends HTMLElement { 
@@ -23,21 +30,27 @@ class LinkList extends HTMLElement {
 		const template = link_list_template.content.cloneNode(true);
 		for(let i = 0; i < this.links.length; i++) {
 			template.appendChild(this.links[i]);
-			template.children[0].appendChild(this.links[i].cloneNode(true));
 		}
 		this.appendChild(template.cloneNode(true));
 		this.querySelector("button").onclick = this.OpenList;
-		this.querySelector("ham-list").style.top = `-${this.querySelector("ham-list").offsetHeight}px`;
 	}
 
 	OpenList() {
-		this.parentElement.classList.toggle("open")
+		this.parentElement.classList.toggle("open");
+
 		if(this.parentElement.classList.contains("open")) {
-			this.parentElement.querySelector("ham-list").style.top = `100%`;
+			const temp = ham_list_temp.content.cloneNode(true);
+			temp.children[0].addEventListener("click", function () {
+				this.parentElement.classList.toggle("open");
+				this.parentElement.querySelector("ham-list").remove();
+			});
+			this.parentElement.appendChild(temp);
+			for(let i = 0; i < this.parentElement.links.length; i++) {
+				this.parentElement.querySelector("ham-list").appendChild(this.parentElement.links[i].cloneNode(true));
+			}
 		} else {
-			this.parentElement.querySelector("ham-list").style.top = `-${this.parentElement.querySelector("ham-list").offsetHeight}px`;
+			this.parentElement.querySelector("ham-list").remove();
 		}
-		
 	}
 }
 
