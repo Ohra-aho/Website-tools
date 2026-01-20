cc73b51e4b86518f2d7e3e624e398dd4bbc25b4e
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-01-20 15:25:17 +0200

    Major grid generator update

--- adjusting/grid.js
+++ adjusting/grid.js
@@ -1,29 +1,55 @@
 
 class GridGenerator extends HTMLElement { 
+	narrow = 500;
+	medium = 1030;
+	wide = 1500;
+
+	cell_height = 0;
+	cell_width = 0;
 
 	constructor() {
 		super();
 	}
 
 	connectedCallback() {
-		const width = this.getAttribute("width") ?? "30%";
-		const height = this.getAttribute("height") ?? "30%";
-		const rows = this.getAttribute("rows") ?? "3";
-		const columns = this.getAttribute("columns") ?? "3";
+		this.cell_width = this.children[0].offsetWidth;
+		this.cell_height = this.children[0].offsetHeight;
+		const parent_width = this.parentElement.offsetWidth;
+		const columns = Math.floor(parent_width / this.cell_width);
+		const rows = Math.floor(this.children.length / columns);
+
 		this.style = 
 		`
-			grid-template-rows: repeat(${rows}, auto);
-			grid-template-columns: repeat(${columns}, auto);
+			grid-template-rows: repeat(${rows}, ${this.cell_height}px);
+			grid-template-columns: repeat(${columns}, ${this.cell_width}px);
 		`;
-		const starting_i = this.children.length;
-		for(let i = 0; i < starting_i; i++) {
-			let cell = document.createElement("div");
-			cell.style = `width: ${width}; height: ${height};`
-			const child = this.children[0];
-			child.style.height = "100%";
-			child.style.width = "auto";
-			cell.appendChild(child);
-			this.appendChild(cell);
+
+		window.addEventListener("resize", () => {
+			console.log("Que")
+			const parent_width = this.parentElement.offsetWidth;
+			console.log(parent_width);
+			const columns = Math.floor(parent_width / this.cell_width);
+			const rows = Math.floor(this.children.length / columns);
+			console.log(columns);
+			console.log(rows);
+
+			this.style = 
+			`
+				grid-template-rows: repeat(${rows}, ${this.cell_height}px);
+				grid-template-columns: repeat(${columns}, ${this.cell_width}px);
+			`;
+			//this.AdjustForScreenWidth(width, height);
+		});
+	}
+
+	AdjustForScreenWidth(width, height) {
+		const window_width = window.innerWidth;
+		
+		if(window_width <= this.medium && window_width > this.narrow) {
+			const cell_width = this.children[0].offsetWidth;
+			if(cell_width < window_width / 2) {
+
+			}
 		}
 	}
 }
